9cd92dca1e8e6d7d671f9bddfe9c8981f6dcb7dc
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-07-16 19:14:16 +0500

    Add Enrollment model and update index.js

---
+++ model/enrollment.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require('../config/dbConnection');
+
+const Enrollment = sequelize.define(
+	'Enrollment',
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			unique: true,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+	},
+	{
+		paranoid: true,
+		tableName: 'Enrollment',
+	}
+);
+
+module.exports = Enrollment;
