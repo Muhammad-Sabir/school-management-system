4da269ee5dde371e7b68fd16513a8b45a13b00a3
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-07-19 20:11:54 +0500

    Add controller for user signin and signup

---
+++ controller/user.js
@@ -0,0 +1,24 @@
+const { login, signup } = require('../service/user');
+
+module.exports = {
+	login: (req, res) => {
+		login(req.query.username, req.query.password)
+			.then((user) => {
+				res.send(user);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.send('Not signing in!');
+			});
+	},
+	signup: (req, res) => {
+		signup(req.body)
+			.then((result) => {
+				res.send(result);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.send('Not signing up!');
+			});
+	},
+};
