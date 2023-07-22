8b3b7b717996dabbdb9afc59c333d21aeba434d2
Muhammad Sabir <sabir.tabi1122@gmail.com>
2023-07-22 19:27:34 +0500

    Finalize student controller

--- controller/studentController.js
+++ controller/studentController.js
@@ -2,9 +2,25 @@ const { studentService } = require('../service/index');
 
 module.exports = {
 	getCourses: (req, res) => {
-		res.send(studentService.getCourses());
+		studentService
+			.getCourses()
+			.then((courses) => {
+				res.send(courses);
+			})
+			.catch((err) => {
+				console.log('Student Controller (getCourses): ', err);
+				res.send('ERROR! Student Controller (getCourses).');
+			});
 	},
 	getTasks: (req, res) => {
-		res.send(studentService.getTasks());
+		studentService
+			.getTasks()
+			.then((tasks) => {
+				res.send(tasks);
+			})
+			.catch((err) => {
+				console.log('Student Controller (getTasks): ', err);
+				res.send('ERROR! Student Controller (getTasks).');
+			});
 	},
 };
